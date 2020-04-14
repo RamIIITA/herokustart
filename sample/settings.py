816c77069dedbd9af3b0ae5bc57572ca991b40f1
RamIIITA <ibi2017005@iiita.ac.in>
2020-04-14 17:38:39 +0530

    conficts

--- sample/settings.py
+++ sample/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+<<<<<<< HEAD
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+=======
+#STATIC_URL = '/static/'
+>>>>>>> refs/remotes/origin/master
